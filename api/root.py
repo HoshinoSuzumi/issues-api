0f41edf79f8be5749930155d516ab637ac51b199
星野鈴美 <master@uniiem.com>
2022-09-20 10:42:23 +0800

    Update root.py

--- api/root.py
+++ api/root.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return redirect('https://github.com/HoshinoSuzumi/issues-api')
+    # return redirect('https://github.com/HoshinoSuzumi/issues-api')
+    return 'GitHub Issues API'
